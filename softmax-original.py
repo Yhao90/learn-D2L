35636d50c7c31833844b7c0fef6b0b7978169e27
yhao <yuanhao22@maoyan.com>
2023-10-08 10:23:52 +0800

    update softmax

--- softmax-original.py
+++ softmax-original.py
@@ -1,7 +1,193 @@
 import torch
 from IPython import display
 from d2l import torch as d2l
+import matplotlib
+import matplotlib.pyplot as plt
 
 # softmax回归的从零开始实现,虽然称为回归但是是用在分类问题上
 batch_size = 256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+# 初始化模型参数 softmax回归输入应该是一个向量，需要展平
+# 权重将构成一个784×10的矩阵， 偏置将构成一个1×10的行向量。 与线性回归一样，我们将使用正态分布初始化我们的权重W，偏置初始化为0。
+num_inputs = 784  # 28*28的训练图像展平
+num_outputs = 10  # 输出总共有10个类别
+W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
+
+# 定义softmax操作,对于任何随机输入，我们将每个元素变成一个非负数。 此外，依据概率原理，每行总和为1。
+def softmax(X):
+    X_exp = torch.exp(X)  # 对每个项求幂（使用exp）
+    partition = X_exp.sum(1, keepdim=True)  # 对每一行求和（小批量中每个样本是一行），得到每个样本的规范化常数
+    return X_exp / partition  # 这里应用了广播机制,将每一行除以其规范化常数，确保结果的和为1。
+
+
+# 定义模型 将数据传递到模型之前，我们使用reshape函数将每张原始图像展平为向量。
+def net(X):
+    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+
+
+y = torch.tensor([0, 2])
+y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+
+
+# y_hat[[0, 1], y] # tensor([0.1000, 0.5000])
+
+
+# 定义交叉熵损失函数
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat[range(len(y_hat)), y])
+
+
+# 求预测准确率，预测正确数/总数
+def accuracy(y_hat, y):  # @save
+    """计算预测正确的数量"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+accuracy(y_hat, y) / len(y)
+
+
+# 同样，对于任意数据迭代器data_iter可访问的数据集， [我们可以评估在任意模型net的精度]。
+# 我们在(Accumulator实例中创建了2个变量， 分别用于存储正确预测的数量和预测的总数量)。 当我们遍历数据集时，两者都将随着时间的推移而累加。
+def evaluate_accuracy(net, data_iter):  # @save
+    """计算在指定数据集上模型的精度"""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式,不求导，只计算backword
+    metric = d2l.Accumulator(2)  # 正确预测数、预测总数
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+# 训练函数
+def train_epoch_ch3(net, train_iter, loss, updater):  # @save
+    """训练模型一个迭代周期（定义见第3章）"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = d2l.Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            # 使用定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练精度，平均损失和正确率
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+# 训练
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):  # @save
+    """训练模型（定义见第3章）"""
+    animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+
+lr = 0.1  # 学习率
+
+
+def updater(batch_size):
+    return d2l.sgd([W, b], lr, batch_size)
+
+
+num_epochs = 5
+# train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+
+
+# 进行预测
+def predict_ch3(net, test_iter, n=6):  # @save
+    """预测标签（定义见第3章）"""
+    for X, y in test_iter:
+        break
+    trues = d2l.get_fashion_mnist_labels(y)
+    preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
+    titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
+    d2l.show_images(
+        X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+
+#预测
+# predict_ch3(net, test_iter)
+
+
+class Accumulator:  # @save
+    """在n个变量上累加"""
+
+    def __init__(self, n):
+        # n=3 [0.0,0.0,0.0]
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class Animator:  # @save
+    """在动画中绘制数据"""
+
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        plt.draw();
+        plt.pause(0.001)
+        display.clear_output(wait=True)
